55fadd57894f8f2722bd27d8ac22dfd1060fdc07
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2025-04-03 08:24:26 +0100

    adding neovim

---
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "clangd","ts_ls" }
+      })
+    end 
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require('lspconfig')
+      
+      -- Define a common on_attach function
+      local on_attach = function(client, bufnr)
+        local opts = { buffer = bufnr, remap = false }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+      end
+
+      -- Setup language servers with capabilities and on_attach
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+        on_attach = on_attach
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach
+      })
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+        on_attach = on_attach
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach
+      })
+    end
+  }
+}
