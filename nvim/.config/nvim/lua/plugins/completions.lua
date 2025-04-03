9e91d8d5dfd0e49ce4b646f1879afb13d515cd35
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2025-04-03 23:32:13 +0100

    fixing dot

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,7 +1,5 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp"
-  },
+  { "hrsh7th/cmp-nvim-lsp" },
   {
     "L3MON4D3/LuaSnip",
     dependencies = {
@@ -9,16 +7,32 @@ return {
       "rafamadriz/friendly-snippets",
     },
   },
+  -- Additional dependencies from lsp-zero
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
   {
     "hrsh7th/nvim-cmp",
     config = function()
-      local cmp = require("cmp")
+      local cmp_status, cmp = pcall(require, "cmp")
+      if not cmp_status then return end
+
+      local luasnip_status, luasnip = pcall(require, "luasnip")
+      if not luasnip_status then return end
+
+      -- Load VSCode-style snippets
       require("luasnip.loaders.from_vscode").lazy_load()
+
+      -- Helper function for SuperTab-like behavior (inspired by lsp-zero)
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
       cmp.setup({
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            luasnip.lsp_expand(args.body) -- Same as lsp-zero
           end,
         },
         window = {
@@ -26,17 +40,76 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
+          -- Your original mappings
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          -- Added from lsp-zero
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
+          { name = "luasnip", keyword_length = 2 }, -- Added keyword_length like lsp-zero
         }, {
+         { name = "buffer", keyword_length = 3 },
+          { name = "path" }, -- Added from lsp-zero
+        }),
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              luasnip = "[Snippet]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+      })
+
+      -- `/` cmdline setup (from lsp-zero)
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
           { name = "buffer" },
+        },
+      })
+
+      -- `:` cmdline setup (from lsp-zero)
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
         }),
       })
     end,
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -3,54 +3,55 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd","ts_ls" }
+        ensure_installed = { "lua_ls", "clangd", "ts_ls", "eslint" }, -- Added eslint
       })
-    end 
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local lspconfig = require('lspconfig')
-      
-      -- Define a common on_attach function
+      local cmp_lsp_status, cmp_lsp = pcall(require, "cmp_nvim_lsp")
+      local capabilities = cmp_lsp_status and cmp_lsp.default_capabilities() or vim.lsp.protocol.make_client_capabilities()
+
+      local lspconfig_status, lspconfig = pcall(require, "lspconfig")
+      if not lspconfig_status then return end
+
+      -- Common on_attach function
       local on_attach = function(client, bufnr)
         local opts = { buffer = bufnr, remap = false }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
       end
 
-      -- Setup language servers with capabilities and on_attach
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-        on_attach = on_attach
+      -- Optional: Configure diagnostics globally
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        update_in_insert = false,
       })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach
-      })
-      lspconfig.eslint.setup({
-        capabilities = capabilities,
-        on_attach = on_attach
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach
-      })
-    end
-  }
+
+      -- Setup language servers
+      local servers = { "clangd", "ts_ls", "eslint", "lua_ls" }
+      for _, server in ipairs(servers) do
+        lspconfig[server].setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+        })
+      end
+    end,
+  },
 }
